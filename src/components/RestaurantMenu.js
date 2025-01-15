f1a5e2d72896eef3e617b4364ba6223a23207797
Shailendra Singh <shailendrasinghraikwar@gmail.com>
2025-01-15 23:29:55 +0530

    Menu card optimized

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,7 +1,7 @@
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
-
+const avgShail = 1;
 const RestaurantMenu = () => {
   const { resId } = useParams();
 
